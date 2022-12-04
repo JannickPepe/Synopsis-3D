c47074f9d81dd686c5872d858e15247d018e905a
jannick <jannickpedersen@live.dk>
2022-12-04 12:33:28 +0100

    setting up the UI and interactive color picker CSS

--- src/App.js
+++ src/App.js
@@ -7,16 +7,48 @@ import { OrbitControls, useGLTF } from '@react-three/drei'; // Allow us to rotat
 
 function App() {
 
-  //construct your reference with useRef method
-  const ref = useRef()
+  //construct your test reference with useRef method
+  //const ref = useRef()
 
   return (
     <div className="App">
+      {/* A test for just simply display a box with set array cords */}
+      {/* 
       <Canvas>
         <mesh ref={ref}>
-          <boxGeometry attach='geometry' argh={[2,2,2]}/> {/* Array position */}
+          <boxGeometry attach='geometry' argh={[2,2,2]}/> 
         </mesh>
       </Canvas>
+      */}
+
+      <div class="wrapper">
+        <div class="card">
+            <div className="product-canvas">
+              <Canvas>
+
+              </Canvas>
+            </div>
+            <h2>Color chooser</h2>
+            <div className='colors'>
+              <div>
+                <input type="color" id="head" name="head" value="#e66465" />
+                    <label for="head">Main</label>
+              </div>
+              <div>
+                <input type="color" id="body" name="body" value="#f6b73c" />
+                    <label for="body">Stripes</label>
+                </div>
+                <div>
+                  <input type="color" id="body" name="body" value="#f6b73c" />
+                    <label for="body">Soul</label>
+                </div>
+                <div>
+                  <input type="color" id="body" name="body" value="#f6b7" />
+                    <label for="body">Laces</label>
+                </div>
+            </div>
+        </div>
+    </div>
     </div>
   );
 }
