07767afd969a2df6263a141cd306fa9eda1cc2c9
jannick <jannickpedersen@live.dk>
2022-12-04 12:55:20 +0100

    added the jsx gltf code and added to canvas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 // After we have gotten the right npm's install and our new bracnhes in package.json
 // Now we import them here
 import './index.css';
-import { useRef, useState } from 'react';
+import { Suspense, useRef, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, useGLTF } from '@react-three/drei'; // Allow us to rotate and import the Models (GLTF, its a format to load 3d models)
+
+// Exported code from the git bash cmd line - npx gltfjsx shoe.gltf
+function Model({ ...props }) {
+  const group = useRef()
+  const { nodes, materials } = useGLTF('/shoe.gltf')
+  // Have the group wrapper around the mesh property for each materials
+  return (
+    <group ref={group} {...props} dispose={null}>
+      <mesh geometry={nodes.shoe.geometry} material={materials.laces} />
+      <mesh geometry={nodes.shoe_1.geometry} material={materials.mesh} />
+      <mesh geometry={nodes.shoe_2.geometry} material={materials.caps} />
+      <mesh geometry={nodes.shoe_3.geometry} material={materials.inner} />
+      <mesh geometry={nodes.shoe_4.geometry} material={materials.sole} />
+      <mesh geometry={nodes.shoe_5.geometry} material={materials.stripes} />
+      <mesh geometry={nodes.shoe_6.geometry} material={materials.band} />
+      <mesh geometry={nodes.shoe_7.geometry} material={materials.patch} />
+    </group>
+  )
+}
 
 function App() {
 
@@ -21,11 +40,15 @@ function App() {
       </Canvas>
       */}
 
-      <div class="wrapper">
-        <div class="card">
+      <div className="wrapper">
+        <div className="card">
             <div className="product-canvas">
               <Canvas>
-
+                {/*When rendering 3D models you need this for error finder/screener*/}
+                <Suspense fallback={null}>
+                  <ambientLight></ambientLight>
+                  <Model></Model>
+                </Suspense>
               </Canvas>
             </div>
             <h2>Color chooser</h2>
@@ -43,7 +66,7 @@ function App() {
                     <label for="body">Soul</label>
                 </div>
                 <div>
-                  <input type="color" id="body" name="body" value="#f6b7" />
+                  <input type="color" id="body" name="body" value="#00FFFF" />
                     <label for="body">Laces</label>
                 </div>
             </div>
